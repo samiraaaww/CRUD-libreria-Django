2946c22c2babca96e51c3cd64cc30307ea777b4f
samira <samyybelen@gmail.com>
2023-06-19 20:02:41 -0400

    Primer commit: Aplicacion funcional de CRUD

---
+++ libreria2/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Libro(models.Model):
+    id = models.AutoField (primary_key=True)
+    titulo = models.CharField(max_length=100, verbose_name='Titulo')
+    imagen = models.ImageField(upload_to='imagenes/', null=True, verbose_name='Imagen')
+    descripcion = models.TextField(null=True, verbose_name='Descripcion')
+
+    def __str__(self):
+        fila= "Titulo: " + self.titulo +" - " + "Descripcion: " + self.descripcion
+        return fila
+
+    def delete(self, using=None, keep_parents=False):
+        self.imagen.storage.delete(self.imagen.name)
+        super().delete()
+
+
